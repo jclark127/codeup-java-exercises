84ee147213d66f3579173ad8e210590ddd6298c0
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-16 13:12:59 -0600

    method exercises

---
+++ src/Bob.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean keepGoing = true;
+        String entry;
+        while (keepGoing) {
+            System.out.println("Say Something to Bob.");
+            entry = sc.nextLine();
+            if (entry.equals("")) {
+                System.out.println("Fine be that way");
+            } else if (entry.charAt(entry.length() - 1) == '?') {
+                System.out.println("Sure");
+            } else if (entry.charAt(entry.length() - 1) == '!') {
+                System.out.println("Whoa, chill out.");
+            } else {
+                System.out.println("Whatever");
+            }
+            System.out.println("Would you like to continue y/n");
+            entry = sc.nextLine();
+            if (entry.equalsIgnoreCase("n")) {
+                keepGoing = false;
+                System.out.println("Program Exited");
+            }
+        }
+    }
+}
---
+++ src/MethodsExercises.java
@@ -0,0 +1,85 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+//        int a = getInteger(1, 10);
+//        int b = getInteger(1, 10);
+//        addition(a,b);
+//        System.out.println();
+//        subtraction(a,b);
+//        System.out.println();
+//        multiplication(a,b);
+//        System.out.println();
+//        division(a,b);
+//        System.out.println();
+//        modulus(a,b);
+        int factorialNumber = getInteger(1, 10);
+        System.out.print(factorialNumber + "! = ");
+        for(int i = 1; i < factorialNumber; i++){
+            System.out.print(i + " x ");
+        }
+        System.out.println(" = " + factorial(factorialNumber));
+    }
+
+    public static void addition(int a, int b) {
+        System.out.println("Your numbers added:");
+        System.out.println(a + b);
+    }
+
+    public static void subtraction(int a, int b) {
+        System.out.println("Your numbers subtracted:");
+        System.out.println(a - b);
+    }
+
+    public static void multiplication(int a, int b) {
+        System.out.println("Your numbers multiplied:");
+        System.out.println(a * b);
+    }
+
+    public static void division(int a, int b) {
+        try {
+            System.out.println("Your numbers divided:");
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+
+    public static void modulus(int a, int b) {
+        try {
+            System.out.println("The modulus of your numbers:");
+            System.out.println(a % b);
+        } catch (Exception e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+
+    public static int getInteger(int min, int max) {
+        boolean keepGoing = true;
+        int number = 0;
+        while (keepGoing) {
+            System.out.println("Enter a number between 1-10");
+            Scanner sc = new Scanner(System.in);
+            try {
+                number = Integer.parseInt(sc.nextLine());
+                if (number < min || number > max) {
+                    System.out.println("Not a valid entry");
+                } else {
+                    keepGoing = false;
+                }
+            } catch (Exception e) {
+                System.out.println("Not a valid entry.");
+            }
+        }
+        return number;
+    }
+
+    public static long factorial(int a) {
+        if (a <= 2) {
+            return a;
+        }
+        return a * factorial(a - 1);
+    }
+}
