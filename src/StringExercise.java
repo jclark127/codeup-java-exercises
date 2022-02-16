ebc345965ff9f3181a40ffcfc5b4907daf577c71
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-16 10:24:34 -0600

    wrapped up string exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String pinkFloyd = "We don't need no education. \nWe don't need no thought control.";
+        System.out.println(pinkFloyd);
+        System.out.println();
+        String exclaim = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(exclaim);
+        System.out.println();
+        String slashes = "In windows, the main drive is ususally C:\\";
+        System.out.println(slashes);
+        System.out.println();
+        String slashy = "I can do backslashes \\, double backslashes\\\\\nand the amazing triple backslashes\\\\\\";
+        System.out.println(slashy);
+    }
+}
