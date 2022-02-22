447d2d0b0f81e305cd94d5a6508d3100dee8beb0
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-22 11:00:30 -0600

    first exercise of java II

---
+++ src/Person.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Person {
+    private String name;
+
+    public Person (String name){
+        this.name = name;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello(){
+        System.out.println("Hello " + name + ".");
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Jon");
+        Person p2 = new Person("Jon");
+
+        System.out.println(p1.getName().equals(p2.getName())); // checks if values are the same
+        System.out.println(p1 == p2); // checks if object is the same
+
+        Person p3 = new Person("Jon"); // new person object
+        Person p4 = p3; // will always return the same value p3
+        System.out.println(p3 == p4); // returns true
+
+        System.out.println(p3.getName()); // returns jon
+        System.out.println(p4.getName()); // returns jon
+        p4.setName("Bill"); //changes value of both p3 and p4
+        System.out.println(p3.getName()); // returns bill
+        System.out.println(p4.getName()); // returns bill
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,79 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner sc = new Scanner(System.in);
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        String str = sc.nextLine();
+        return str;
+    }
+
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String yorn = sc.nextLine();
+        if (yorn.equalsIgnoreCase("y")) {
+            return true;
+        } else if (!yorn.equalsIgnoreCase("n")) {
+            System.out.println("Please enter a valid selection");
+            yesNo(prompt);
+        }
+        return false;
+    }
+
+    public int getInt(int min, int max, String prompt) {
+        boolean keepGoing = true;
+        int n1 = 0;
+        while (keepGoing) {
+            System.out.println(prompt);
+            n1 = sc.nextInt();
+            if (n1 > max || n1 < min) {
+                System.out.println("Please enter a valid choice");
+            } else {
+                keepGoing = false;
+            }
+        }
+        return n1;
+    }
+
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        int n1 = sc.nextInt();
+        return n1;
+    }
+
+    public double getDouble(double min, double max, String prompt) {
+        boolean keepGoing = true;
+        double n1 = 0.00;
+        while (keepGoing) {
+            System.out.println(prompt);
+            n1 = Double.parseDouble(sc.nextLine());
+            if (n1 > max || n1 < min) {
+                System.out.println("Please enter a valid choice");
+            } else {
+                keepGoing = false;
+            }
+        }
+        return n1;
+    }
+
+    public double getDouble(String prompt) {
+        sc.nextLine();
+        System.out.println(prompt);
+        double n1 = Double.parseDouble(sc.nextLine());
+        return n1;
+    }
+
+    public static void main(String[] args) {
+        Input sc = new Input();
+        System.out.println(sc.getString("Please enter a string."));
+        System.out.println(sc.yesNo("Enter y or n."));
+        System.out.println(sc.getInt("Enter an integer."));
+        System.out.println(sc.getInt(1, 10, "Enter an integer between 1 and 10."));
+        System.out.println(sc.getDouble("Enter a Double."));
+        System.out.println(sc.getDouble(1.0, 10.0, "Enter a double between 1.0 and 10.0"));
+    }
+
+}
