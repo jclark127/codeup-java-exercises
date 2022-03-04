90bdf4ae419c62d61cc6c890589f74258fd2a541
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-04 16:12:57 -0600

    progress on grocery list app

--- src/GroceryList/GroceryList.java
+++ src/GroceryList/GroceryList.java
@@ -11,7 +11,7 @@ public class GroceryList {
 
     public static void main(String[] args) {
         groceryList.put(createListItem(), sc.getInt("Enter quantity."));
-        groceryList.forEach(((listItem, integer) -> System.out.println(listItem.item + " " + listItem.category + " " + integer)));
+        viewList();
     }
 
     public static void mainMenuPrompt() {
@@ -24,6 +24,16 @@ public class GroceryList {
         System.out.println();
     }
 
+    public static void modifySubMenu() {
+        System.out.println();
+        System.out.println("1. Change Item Quantity.");
+        System.out.println("2. Change Name of item.");
+        System.out.println("3. Change Item Category.");
+        System.out.println("4. Remove Item.");
+        System.out.println("5. Return to main menu.");
+        System.out.println();
+    }
+
     public static void runProgram() {
         boolean keepGoing = true;
         String entry = "";
@@ -33,11 +43,13 @@ public class GroceryList {
             switch (entry) {
                 case "1":
                     boolean goOn = true;
-//                    while (goOn){
-//                    createListItem();
-//                    break;
+                    while (goOn) {
+                        createListItem();
+                        goOn = sc.yesNo("Add another item?");
+                    }
+                    break;
                 case "2":
-                    //viewList();
+                    viewList();
                     break;
                 case "3":
                     //modifyListItem();
@@ -54,6 +66,47 @@ public class GroceryList {
 
     public static ListItem createListItem() {
         String item = "";
+        String category = getCategory();
+//        String category = "";
+//        for (int i = 0; i < categories.length; i++) {
+//            System.out.print(i + 1 + ". " + categories[i] + " ");
+//        }
+//        System.out.println();
+//        System.out.println();
+//        String entry = sc.getString("Select a category.");
+//        switch (entry) {
+//            case "1":
+//                category = categories[0];
+//                break;
+//            case "2":
+//                category = categories[1];
+//                break;
+//            case "3":
+//                category = categories[2];
+//                break;
+//            case "4":
+//                category = categories[3];
+//                break;
+//            case "5":
+//                category = categories[4];
+//                break;
+//            default:
+//                System.out.println("That is not one of the choices.");
+//                break;
+//        }
+        item = sc.getString("Enter the name of the item you are adding.");
+        return new ListItem(item, category);
+    }
+
+    public static void viewList() {
+        groceryList.forEach(((listItem, integer) -> System.out.println(listItem.item + " " + listItem.category + " " + integer)));
+    }
+
+    public static void modifyListItem() {
+
+    }
+
+    public static String getCategory(){
         String category = "";
         for (int i = 0; i < categories.length; i++) {
             System.out.print(i + 1 + ". " + categories[i] + " ");
@@ -61,7 +114,6 @@ public class GroceryList {
         System.out.println();
         System.out.println();
         String entry = sc.getString("Select a category.");
-        item = sc.getString("Enter the name of the item you are adding.");
         switch (entry) {
             case "1":
                 category = categories[0];
@@ -82,7 +134,7 @@ public class GroceryList {
                 System.out.println("That is not one of the choices.");
                 break;
         }
-        return new ListItem(item, category);
+        return category;
     }
 
 }
