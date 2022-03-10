7ba7149421dc35b0784dfb711d967b755395ca7b
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-10 11:28:33 -0600

    manipulating lists from files

--- src/fileIO/LectureEnvironment.java
+++ src/fileIO/LectureEnvironment.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LectureEnvironment {
@@ -39,5 +42,35 @@ public class LectureEnvironment {
             Files.createDirectories(dataDirectory);
             Files.createFile(dataAndFile);
         }
+
+        System.out.println();
+        System.out.println("dataAndFile = " + dataAndFile);
+
+
+        List<String> groceries = Arrays.asList("coffee", "whiskey", "steak", "potatoes","candy");
+        System.out.println("groceries = " + groceries);
+
+        Files.write(dataAndFile,groceries);
+
+        List<String> forgot = Arrays.asList("cheese","otherCheese","thirdCheese");
+        Files.write(dataAndFile,forgot, StandardOpenOption.APPEND);
+
+        List<String> printList = Files.readAllLines(dataAndFile);
+        System.out.println("printList = " + printList);
+        System.out.println();
+        for(int i = 0; i < printList.size(); i++){
+            System.out.printf("%d. %s%n", i+1,printList.get(i));
+        }
+
+        List<String> printList2 = Files.readAllLines(dataAndFile);
+        List<String> newList = new ArrayList<>();
+        for(String line : printList2){
+            if(line.equalsIgnoreCase("cheese")){
+                newList.add("vegan cheese");
+                continue;
+            }
+            newList.add(line);
+        }
+        System.out.println("newList = " + newList);
     }
 }
